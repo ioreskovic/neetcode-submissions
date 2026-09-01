e19743b9c440d6fe21f28616f409eb3f1e841fc4
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2026-09-02 00:30:36 +0200

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.java
@@ -0,0 +1,22 @@
+class LRUCache {
+    private final LinkedHashMap<Integer, Integer> map;
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new LinkedHashMap<>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+    
+    public int get(int key) {
+        return this.map.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
