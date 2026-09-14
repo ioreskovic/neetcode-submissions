9def6eb7f3c420f81c0415bca3dd88d9252ea021
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2026-09-14 18:30:09 +0200

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            char left = s.charAt(l);
+            if (!Character.isLetterOrDigit(left)) {
+                l++;
+                continue;
+            }
+
+            char right = s.charAt(r);
+            if (!Character.isLetterOrDigit(right)) {
+                r--;
+                continue;
+            }
+
+            if (Character.toLowerCase(left) == Character.toLowerCase(right)) {
+                l++;
+                r--;
+                continue;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
